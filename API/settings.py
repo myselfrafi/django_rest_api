383ad9f3e7a4eb856929e58453891b485eaa54d2
myselfrafi <shaiksayedrafishaik@gmail.com>
2026-03-13 12:40:17 +0530

    Django REST API

--- API/settings.py
+++ API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&dv#rm+0c4-j-tdplu01u3$w%3#x_lecg2zlkb_^tx&m_$jivy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
